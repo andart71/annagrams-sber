c66b11dc7ebc18bef209bc4ac85ae271e5052ce0
Andrey Loshkarev <58550368+andart71@users.noreply.github.com>
2022-04-05 12:31:23 +0300

    Update assistant.tsx

--- src/assistant.tsx
+++ src/assistant.tsx
@@ -6,7 +6,7 @@ const initializeAssistant = (getState: any) => {
     if (process.env.NODE_ENV === 'development') {
         return createSmartappDebugger({
             // Токен из Кабинета разработчика
-            token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwNGQwN2IwNmMzMGVlYjc3YzRlNjMxNDcxMzA0YWM2ZTc3MTAxOWU3Nzk2YTQyYjg2M2ZkMWM3MmRhYWIwOTg5NTM5YmU5MjcwMDQyNjI5OCIsImF1ZCI6IlZQUyIsImV4cCI6MTY0NzY3MTg2MSwiaWF0IjoxNjQ3NTg1NDUxLCJpc3MiOiJLRVlNQVNURVIiLCJ0eXBlIjoiQmVhcmVyIiwianRpIjoiNTQ0M2Q1OWItYmRlMC00NDljLWEyMGUtNmNlZmVkY2QxMmEzIiwic2lkIjoiNzBiYWI1NGYtODRmMC00YWQ0LWI0NGUtM2E2ZDc3YzRkNzg2In0.vBL_LFRg71pyXBOGtIgfvcKkzTYcVAi0SjbRqnGFBad7EhsBzpbQ1Mz-4PfxQVMnoSCqToKTyRphoL9SC36Y2kqF0HEPu0_VpLN8Ps4UfTNxhtE94mXTt20lw3uYCvNxT8N1aMQTiVC4wwKNEQ3VAT_qtdBRw09ARXtpqPaPNvII-xsdf3cX2HXUALndzqyi1GWuoGOiHrWaOeoJMd47qyVWyYleVTBYj6_G9tDLzVY_ytgOpsaYeeEzFBx-wbu7pOj2oax2GPE9VRAmTiF2DBzaclS04QtoDtf1P7m-r5oZRjWpoUXShGaUlwwZWduGGElsLozxYD8-i2Qv7upEq9EJKsjKdUCPp_jGFcUP03RWW2yREiYgTKcSaSiMLEvIm0XVzdyvDmwrFoBvxsIHtRupfrnut8IPgRbxQJyyubEpF7_tRTB1T1KIC7TFdnBZHzjcuDKctWofefLsPosCY2Twf82y3GJduR6n4dPR8ZJHxLPStOXENqUgcbXXpfiGkh5A3E2txtHPH7cGA_TB-0GdLsM8TlAdQBm_fagYFfF8dHrqmp_fdHhF12TOx0thfW8TN61zavTa6hKpOjGr45zt6PsVDoCKaqAlasZ_AlWIRKM5mwIH9oHHl9V1BRiqNb1_CDQoZWvp-fX8DZ_CZXxy5VKDiCBE7m0N_b7Bz8E',
+            token: '',
             initPhrase: 'запусти workshop canvas',
             // Текущее состояние смартапа
             getState
